41c3f4dd64a417a2e19605602116dca04a73d44c
mamanzano <tesidhc@gmail.com>
2022-09-30 00:00:02 -0500

    extra point

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, session
 
 app = Flask(__name__)
 
-@app.route("/")
+app.secret_key = 'secret_key'
+
+@app.route("/", methods = ['GET', 'POST'])
 def hello():
-    return "Hello World!!!"
+
+    if session.get('logged_in') == None:
+        count = 0
+    else:
+       count = int(session['logged_in']) + 1
+
+    if request.method == 'GET':
+        return f"Hello World!!! Tenemos: {count} Peticiones"
+    if request.method == 'POST':
+        session['logged_in'] = count 
+        return {'Msg':'success post'}
+
+
 
 
 if __name__ == "__main__":
